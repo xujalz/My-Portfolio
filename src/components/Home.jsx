e0be35f958b50d867a0c0a76fae5ee5272a19206
Sujal Gupta <sujalgupta1104@gmail.com>
2023-08-31 10:33:39 +0530

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row pt-10">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Full Stack Developer!
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I'm a final Year Student having experience of building and desgining Web Application.
